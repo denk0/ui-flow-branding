0166dff254b69a6c48cc082f06c0b5fc1913cbf1
Vlad Romanenko <denko1024@gmail.com>
2017-01-31 17:12:23 +0200

    Add single project main page

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,10 @@
-$(document).foundation();
+;(()=> {
+	$(document).foundation();
+
+	$(window).on('load', () => {
+		$('button[data-toggle="project-navigation"]').on('click', ()=> {
+			$('.secondary-navigation').toggleClass('opened');
+			$('.main-content').toggleClass('navigation-opened');
+		});
+	});
+})();
